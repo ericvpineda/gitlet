bb0689aae98c3e48c56be3896e2a255f56906a98
Eric Pineda <ericvpineda@berkeley.edu>
2023-03-31 15:06:38 -0700

    updated -- GitletObject.java comments

--- gitlet/GitletObject.java
+++ gitlet/GitletObject.java
@@ -7,16 +7,24 @@ import java.io.Serializable;
 /* Interface to save objects to disk */
 public interface GitletObject extends Serializable {
 
+    /** Helper function that will call overloaded write call in GitletObject */
     void write() throws IOException;
 
+    /** Create SHA1 for file or folder */
     String createSha1();
 
     /** Writes content into Main.objects file */
     default void write(String sha1, Object contents, File location) throws IOException {
+
+        // Create folder based on first two characters of file identifier
         File innerFile = Utils.join(location,sha1.substring(0,2));
         innerFile.mkdir();
+
+        // Create file inside of previously created folder
         File loc = Utils.join(location,sha1.substring(0,2),sha1.substring(2));
         loc.createNewFile();
+
+        // Write file contents to disk
         Utils.writeObject(loc, (Serializable) contents);
     }
 }
