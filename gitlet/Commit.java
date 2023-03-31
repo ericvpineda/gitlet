6127eb79f0c3312db89f7fb28d40f33e65592424
Eric Pineda <ericvpineda@berkeley.edu>
2023-03-31 14:31:17 -0700

    udpated -- comments for Commit class

--- gitlet/Commit.java
+++ gitlet/Commit.java
@@ -5,73 +5,72 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
 
-// Note:
-// - commit can be null, but not its contents
-
 /* Class for commit command */
 public class Commit implements GitletObject, Serializable {
-    public static String zeroSha1 = "0000000000000000000000000000000000000000";
-    String _parentSha1;
-    String _logMessage;
-    String _tree;
-    String _sha1;
-    String _time;
-    String _mergedId;
+    public static String zeroSha1 = "0000000000000000000000000000000000000000"; // Initial gitlet commit identifier
+    String _parentSha1;     // Parent commit identifier
+    String _logMessage;     // Commit message
+    String _tree;           // Tree object identifier
+    String _sha1;           // Commit identifier
+    String _time;           // Timestamp in -- [Day] [Month] [Date] 00:00:00 [Year] format
+    String _mergedId;       // Merge commit identifier
 
     /**
      * Commit constructor
      */
     public Commit(String msg, String parentSha1) throws IOException {
-        _parentSha1 = parentSha1;
-        _logMessage = msg;
-        _time = Utils.createTime();
-        _tree = createTree();
-        _sha1 = createSha1();
-        _mergedId = null;
+        _parentSha1 = parentSha1;   // Parent identifier
+        _logMessage = msg;          // Commit message
+        _time = Utils.createTime(); // Time commit created
+        _tree = createTree();       // Commit tree identifier
+        _sha1 = createSha1();       // Commit identifier
+        _mergedId = null;           // Merge commit identifier
     }
 
     // Note: Create a deep copy of commit (Rebase command)
     public Commit(Commit deepCopy, String parentSha1, String tree) {
-        _parentSha1 = parentSha1;
-        _logMessage = deepCopy._logMessage;
-        _time = Utils.createTime();
-        _tree = tree;
-        _sha1 = createSha1();
-        _mergedId = null;
+        _parentSha1 = parentSha1;            // Parent identifier
+        _logMessage = deepCopy._logMessage;  // Commit message
+        _time = Utils.createTime();          // Time commit created
+        _tree = tree;                        // Commit tree identifier
+        _sha1 = createSha1();                // Commit identifier
+        _mergedId = null;                    // Merge commit identifier
     }
 
-    // Note: Create a deep copy of commit for remote
+    // Note: Create a deep copy of commit (For remote)
     public Commit(Commit deepCopy, String parent) {
-        _parentSha1 = parent;
-        _logMessage = deepCopy._logMessage;
-        _time = deepCopy._time;
-        _tree = deepCopy._tree;
-        _sha1 = createSha1();
-        _mergedId = deepCopy._mergedId;
+        _parentSha1 = parent;                // Parent identifier
+        _logMessage = deepCopy._logMessage;  // Commit message
+        _time = deepCopy._time;              // Time commit created
+        _tree = deepCopy._tree;              // Commit tree identifier
+        _sha1 = createSha1();                // Commits identifier
+        _mergedId = deepCopy._mergedId;      // Merge commit identifier
     }
 
+    /** Constructor to create commit based on file directory */
     public Commit(String msg, String parentSha1, File dir) throws IOException {
-        _parentSha1 = parentSha1;
-        _logMessage = msg;
-        _time = Utils.createTime();
-        _tree = createTree(dir);
-        _sha1 = createSha1();
-        _mergedId = null;
+        _parentSha1 = parentSha1;       // Parent identifier
+        _logMessage = msg;              // Commit message
+        _time = Utils.createTime();     // Time commit created
+        _tree = createTree(dir);        // Commit tree identifier
+        _sha1 = createSha1();           // Commits identifier
+        _mergedId = null;               // Merge commit identifier
     }
 
-    /** Create Tree object */
+    /** Create empty Tree object */
     public String createTree() throws IOException {
         Tree tree = new Tree();
         return tree._sha1;
     }
 
+    /** Create Tree object based on file directory */
     public String createTree(File dir) throws IOException {
         Tree tree = new Tree(dir);
         return tree._sha1;
     }
 
     /**
-     * Create commit sha1
+     * Create commit SHA1
      */
     public String createSha1() {
         if (_parentSha1 == null) {
@@ -82,7 +81,7 @@ public class Commit implements GitletObject, Serializable {
     }
 
     /**
-     * Serialize and update the branch based on the commit sha1
+     * Serialize and update the branch based on the commit SHA1
      */
     public void write() throws IOException {
         Commit curr = Commit.getCurrent();
@@ -102,9 +101,8 @@ public class Commit implements GitletObject, Serializable {
         write(_sha1, this, commitFile);
     }
 
-    // Note: will never be null
     /**
-     * Get the current commit
+     * Get the HEAD commit of the current branch
      */
     public static Commit getCurrent()  {
         String commitName = Commit.getCurrentSha1();
@@ -112,7 +110,7 @@ public class Commit implements GitletObject, Serializable {
     }
 
     /**
-     * Get the current commit
+     * Get the current commit SHA1
      */
     public static String getCurrentSha1() {
         if (Main.INDEX.length() == 0) {
@@ -122,11 +120,13 @@ public class Commit implements GitletObject, Serializable {
         return Utils.deserialize(Utils.join(Main.BRANCH, branch), String.class);
     }
 
+    /** Get list of blobs from current commit */
     public static HashMap<String,String> getCurrentBlobs() {
         Commit commit = getCurrent();
         return getBlobs(commit._tree);
     }
 
+    /** Get list of blobs from given tree SHA1 */
     public static HashMap<String,String> getBlobs(String treeSha1) {
         File sectreeFile = Utils.findFile(treeSha1, Main.TREE);
         Tree tree = Utils.deserialize(sectreeFile, Tree.class);
