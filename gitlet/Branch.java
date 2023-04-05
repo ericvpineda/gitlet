e25777f5f80a32d44856633315dbcd1ce32c8a47
Eric Pineda <ericvpineda@berkeley.edu>
2023-04-05 14:17:40 -0700

    updated -- Branch.java comments

--- gitlet/Branch.java
+++ gitlet/Branch.java
@@ -11,60 +11,18 @@ import java.io.Serializable;
 public class Branch implements Serializable {
 
     /**
-     * Write new branch file to disk using branch name (if applicable).
-     */
-    public static void write(String name) throws IOException {
-
-        // Verify that given name does not already exist
-        File file = Utils.join(Main.BRANCH, name);
-        if (file.exists()) {
-            System.out.print("A branch with that name already exists.");
-            return;
-        }
-
-        // Create new branch file and write to disk
-        file.createNewFile();
-
-        // Update current commit with new branch file
-        String currentCommitID = Commit.getCurrentID();
-        update(currentCommitID, name, Main.BRANCH);
-    }
-
-    /**
-     * Update current branch with commit id
-     */
-    public static void update(String commitID, String branch, File file) {
-        Utils.writeContents(Utils.join(file, branch), commitID);
-    }
-
-    /**
-     * Saves the current branch name as HEAD pointer
-     */
-    public static void writeHead(String name, File file)  {
-        File branch = Utils.join(file);
-        if (name.contains("/")) {
-            name = name.substring(name.indexOf("/") + 1);
-        }
-        Utils.writeContents(branch, name);
-    }
-
-    /**
      * Get current branch name
      */
     public static String getCurrentName() {
         return Utils.readContentsAsString(Main.HEAD);
     }
 
-    /**
-     * Get HEAD commit identifier of given branch
-     */
-    public static String read(String branchName) {
-        File branchFile = Utils.createFilePath(branchName, Main.BRANCH);
-        if (branchFile != null) {
-            return Utils.readContentsAsString(branchFile);
-        }
-        return null;
 
+    /**
+     * Update current branch with commit id
+     */
+    public static void update(String commitID, String branch, File file) {
+        Utils.writeContents(Utils.join(file, branch), commitID);
     }
 
     /**
@@ -98,5 +56,47 @@ public class Branch implements Serializable {
 
         }
         System.out.println();
+    }
+
+    /**
+     * Get HEAD commit identifier of given branch
+     */
+    public static String read(String branchName) {
+        File branchFile = Utils.createFilePath(branchName, Main.BRANCH);
+        if (branchFile != null) {
+            return Utils.readContentsAsString(branchFile);
+        }
+        return null;
+    }
+
+    /**
+     * Write new branch file to disk using branch name (if applicable).
+     */
+    public static void write(String name) throws IOException {
+
+        // Verify that given name does not already exist
+        File file = Utils.join(Main.BRANCH, name);
+        if (file.exists()) {
+            System.out.print("A branch with that name already exists.");
+            return;
+        }
+
+        // Create new branch file and write to disk
+        file.createNewFile();
+
+        // Update current commit with new branch file
+        String currentCommitID = Commit.getCurrentID();
+        update(currentCommitID, name, Main.BRANCH);
+    }
+
+    /**
+     * Saves the current branch name as HEAD pointer
+     */
+    public static void writeHead(String name, File file)  {
+        File branch = Utils.join(file);
+        if (name.contains("/")) {
+            name = name.substring(name.indexOf("/") + 1);
+        }
+        Utils.writeContents(branch, name);
     }
 }
