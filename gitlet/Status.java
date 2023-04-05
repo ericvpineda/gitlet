75013fd183450822ce8d62cbc56438852870ecd8
Eric Pineda <ericvpineda@berkeley.edu>
2023-04-05 15:23:37 -0700

    updated -- State.java comments and updated ambiguous variable names

--- gitlet/Status.java
+++ gitlet/Status.java
@@ -9,13 +9,18 @@ public class Status {
     static Stage stage = Stage.read();
 
     /**
-     * Constructor for Status
+     * Constructor for Status command
      */
     public static void print() throws IOException {
+        // Print all branches created, but not removed
         printBranches();
+        // Print all files staged for addition
         printStagedFiles();
+        // Print all files staged for deletion
         printRemovedFiles();
+        // Print all files modified since current commit
         printModifications();
+        // Print all files untracked in current commit
         printUntracked();
     }
 
@@ -25,7 +30,10 @@ public class Status {
     private static void printBranches() {
         System.out.println("=== Branches ===");
         String curr = Branch.getCurrentName();
+
+        // Note: Branch directory never null since created during initialization
         for (File file : Main.BRANCH.listFiles()) {
+            // Add symbol if branch name is current branch
             if (file.getName().equals(curr)) {
                 System.out.println("*" + file.getName());
             } else {
@@ -51,12 +59,11 @@ public class Status {
     }
 
     /**
-     * Print files that are currently marked for removal.
+     * Print files that are currently staged for removal.
      */
     private static void printRemovedFiles() {
         System.out.println("=== Removed Files ===");
         if (!stage._deletions.isEmpty()) {
-            // Note: iterate through Main.HISTORY of branch
             Iterator iter = stage._deletions.entrySet().iterator();
             for (Iterator it = iter; it.hasNext(); ) {
                 Map.Entry obj = (Map.Entry) it.next();
@@ -73,37 +80,41 @@ public class Status {
      */
     private static void printModifications() {
         System.out.println("=== Modifications Not Staged For Commit ===");
-        // Note: curr branch history
+
+        // Get all files from current commmit
         HashMap<String,String> currBlobList = Commit.getCurrentBlobs();
+
+        // Iterate through files from current commit
         Iterator iter = currBlobList.entrySet().iterator();
         for (Iterator it = iter; it.hasNext(); ) {
             Map.Entry obj = (Map.Entry) it.next();
-            String blobName = (String) obj.getKey();
-            String blobSHA1 = (String) obj.getValue();
-            String cwdSha1 = Utils.findByFileName(blobName);
-            // 1. Tracked currCom, changed in CWD, not staged
-            if (cwdSha1 != null && !blobSHA1.equals(cwdSha1) &&
-                !stage._additions.containsValue(cwdSha1)) {
-                System.out.println(blobName + " (modified)");
-                // 2. tracked currCom, not staged for removal, deleted in CWD
+            String fileName = (String) obj.getKey();
+            String committedFileHash = (String) obj.getValue();
+            String workingDirectoryFileHash = Utils.findByFileName(fileName);
+            // 1. Tracked in current commit, changed in current working directory, but not staged
+            if (workingDirectoryFileHash != null && !committedFileHash.equals(workingDirectoryFileHash) &&
+                !stage._additions.containsValue(workingDirectoryFileHash)) {
+                System.out.println(fileName + " (modified)");
             }
-            if (cwdSha1 == null && !stage._deletions.containsKey(blobName)){
-                System.out.println(blobName + " (deleted)");
+            // 2. tracked current commit, not staged for removal, deleted in CWD
+            if (workingDirectoryFileHash == null && !stage._deletions.containsKey(fileName)){
+                System.out.println(fileName + " (deleted)");
             }
         }
+        // Iterate through files stage for addition
         iter = stage._additions.entrySet().iterator();
         for (Iterator it = iter; it.hasNext(); ) {
             Map.Entry obj = (Map.Entry) it.next();
-            String blobName = (String) obj.getKey();
-            String blobSha1 = (String) obj.getValue();
-            String fileSHA1 = Utils.findByFileName(blobName);
-            // 2. Staged, different content in CWD
-            if (fileSHA1 != null && !fileSHA1.equals(blobSha1)) {
-                System.out.println(blobName + " (modified)");
-            // 3. staged, deleted in CWD
+            String fileName = (String) obj.getKey();
+            String committedFileHash = (String) obj.getValue();
+            String workingDirectoryFileHash = Utils.findByFileName(fileName);
+            // 2. File staged, but different contents in current working directory
+            if (workingDirectoryFileHash != null && !workingDirectoryFileHash.equals(committedFileHash)) {
+                System.out.println(fileName + " (modified)");
             }
-            if (fileSHA1 == null) {
-                System.out.println(blobName + " (deleted)");
+            // 3. File staged, but deleted in current working directory
+            if (workingDirectoryFileHash == null) {
+                System.out.println(fileName + " (deleted)");
             }
         }
         System.out.println();
@@ -115,20 +126,23 @@ public class Status {
     static void printUntracked() {
         System.out.print("=== Untracked Files ===");
         ArrayList<String> filesToIgnore = Utils.IGNORE_FILES;
+        // Get all files from current commit
         HashMap<String, String> trackedFiles = Commit.getCurrentBlobs();
-        // Note: iterating over user dir
+        // Note: iterating over user directory
         for (File file : Main.USERDIR.listFiles()) {
             String fileName = file.getName();
+            // Ignore files from files to ignore
             if (filesToIgnore.contains(fileName) ||
                 file.isDirectory()) {
                 continue;
             }
+            // Create temporary file to extract name and hash
             Blob temp = new Blob(file);
-            // Note: file staged for removal by recreated without gitlet knowledge
-            // && !stage._deletions.containsKey(fileName)
+            // Check if file name exists in tracked files
             if (trackedFiles.containsKey(temp._name)) {
                 continue;
             }
+            // Check if file is already staged for addition
             if (stage._additions.containsValue(temp._sha1)) {
                 continue;
             }
